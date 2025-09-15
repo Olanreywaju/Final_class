71868d6735578ec910cfa0ef2af8d94e40329191
Olanrewaju <emmanuelspatt@gmail.com>
2025-09-15 15:52:26 +0100

    i have edited the controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ exports.register = async (req, res) => {
         const exisistingEmail = await userModel.findOne({email})
         const existingPhoneNumber = await userModel.findOne({phoneNumber})
         if (exisistingEmail || existingPhoneNumber) {
-            res.status(400).json({message: 'User already exist'})
+           return res.status(400).json({message: 'User already exist'})
         }
         const saltRound = await bcrypt.genSalt(10);
         const hashPassword = await bcrypt.hash(password, saltRound);
